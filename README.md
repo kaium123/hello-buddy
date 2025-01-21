66008a5b2a53cb274b185e3df5e4d44bc7c9cf87
mohammad.kaium <mohammad.kaium@technonext.com>
2025-01-21 13:48:43 +0600

    modify script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hello-buddy
 # hello-buddy
+
+# install hello using command below
+
+curl https://raw.githubusercontent.com/kaium123/hello-buddy/main/install.sh -o install.sh \
+&& chmod +x install.sh \
+&& sudo ./install.sh
--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-nohup ./hello > /dev/null 2>&1 &
+#!/bin/bash
+
+# Ensure /usr/local/bin exists and has correct permissions
+sudo chmod 755 /usr/local/bin
+
+# Download the hello binary to /usr/local/bin
+echo "Downloading the hello binary..."
+sudo curl -L -o /usr/local/bin/hello https://github.com/kaium123/hello-buddy/releases/download/v1/hello
+
+# Make the hello binary executable
+sudo chmod +x /usr/local/bin/hello
+
+# Optionally, you can run it in the background (detached mode)
+nohup /usr/local/bin/hello > /dev/null 2>&1 &
+
+echo "Installation complete!"
